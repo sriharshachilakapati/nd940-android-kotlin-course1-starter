2cb3e69fe7381ac95244e0fd316bc0d11f50575e
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-10-30 21:22:14 +0530

    Hide the ActionBar for LoginFragment

--- solution/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ solution/app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -1,6 +1,19 @@
 package com.udacity.shoestore.ui.login
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.udacity.shoestore.R
 
-class LoginFragment : Fragment(R.layout.fragment_login)
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as? AppCompatActivity)?.supportActionBar?.show()
+    }
+}
