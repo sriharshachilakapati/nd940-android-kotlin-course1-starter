aa5283437065fb2adc465b56f157b0246e06a3ab
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-10-30 16:00:32 +0530

    Convert Main Activity to a data binding layout and configure action bar with Navigation UI

--- solution/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ solution/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,33 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            binding.navHostFragment.findNavController()
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        binding.navHostFragment.findNavController().navigateUp() || super.onSupportNavigateUp()
 }
