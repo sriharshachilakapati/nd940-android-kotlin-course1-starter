0b78f6f92bc7c34c7648c3b5f6838958145dd675
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2020-10-31 22:57:46 +0530

    Extracted TitleText style and implemented WelcomeFragment

---
+++ solution/app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeFragment.kt
@@ -0,0 +1,53 @@
+package com.udacity.shoestore.ui.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
+import timber.log.Timber
+
+class WelcomeFragment : Fragment() {
+    private val viewModel: WelcomeViewModel by viewModels()
+
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        with(binding) {
+            onFabClicked = View.OnClickListener { onFloatingActionButtonClicked() }
+
+            viewModel = this@WelcomeFragment.viewModel.apply {
+                userName = "Harsha"
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as? AppCompatActivity)?.supportActionBar?.show()
+    }
+
+    private fun onFloatingActionButtonClicked() {
+        Timber.d("Hello World")
+        Toast.makeText(activity, "Hello", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ solution/app/src/main/java/com/udacity/shoestore/ui/welcome/WelcomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.udacity.shoestore.ui.welcome
+
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+    var userName: String = "undefined"
+}
